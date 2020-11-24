0b2b9bd74ec8d96be13f00d5eb67d9b1b183fd2a
Gabrysia <gfraczek1999@gmail.com>
2020-11-24 13:05:43 +0100

    dashboard connected with backend + style change

--- client-side/src/app/dashboard/dashboard.component.ts
+++ client-side/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../core/user/user.service";
+import {MatDialog} from "@angular/material/dialog";
+import { BookshelvesComponent} from "../bookshelves/bookshelves.component";
+import {BookshelfService} from "../core/bookshelves/bookshelf.service";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent extends BookshelvesComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(protected userService: UserService, protected dialog: MatDialog, protected bookshelfService: BookshelfService) {
+    super(dialog, userService, bookshelfService)
   }
 
 }
