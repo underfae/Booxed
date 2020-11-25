e0d39d3770741fa3b4edab6fb4de79dd55b5f85e
Gabrysia <gfraczek1999@gmail.com>
2020-11-25 17:16:30 +0100

    orders dialog

--- client-side/src/app/shared/components/orders-dialog/orders-dialog.component.ts
+++ client-side/src/app/shared/components/orders-dialog/orders-dialog.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+
+import {Library} from "../../../core/points/library/library.model";
+import {Order} from "../../../core/points/order/order.model";
+import {Reward} from "../../../core/points/reward/reward.model";
 
 @Component({
   selector: 'app-orders-dialog',
   templateUrl: './orders-dialog.component.html',
   styleUrls: ['./orders-dialog.component.scss']
 })
-export class OrdersDialogComponent implements OnInit {
+export class OrdersDialogComponent {
 
-  orders=[1,2,3,4,5,6,3,5,67,23];
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: orderDialogData) {
   }
+
+  getLibraryAddress(id: string): string {
+    let filteredItem = this.data.libraries.filter(x => x._id === id)
+    return filteredItem[0].street + ' ' + filteredItem[0].building_number + '/' + filteredItem[0].apartment_number;
+  }
+
+  getLibraryName(id: string): string {
+    return this.data.libraries.filter(x => x._id === id)[0].name
+  }
+
+  getRewardName(id: string): string {
+    return this.data.rewards.filter(x => x._id === id)[0].name
+  }
+
+}
+
+export interface orderDialogData {
+  userId: string;
+  libraries: Library[];
+  rewards: Reward[];
+  orders: Order[];
 
 }
