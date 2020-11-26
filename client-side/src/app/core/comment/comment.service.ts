9eaaedc1793692ca96391229dda80d798110fd34
Gabrysia <gfraczek1999@gmail.com>
2020-11-26 21:25:57 +0100

    environment.ts Google Books api key added

---
+++ client-side/src/app/core/comment/comment.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {Comment, UpdatedComment} from "./comment.model";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(protected http: HttpClient) { }
+
+  getComments(id :string){
+    return this.http.get(environment.apiBaseUrl + '/comments/'+ id)
+  }
+  updateComment(updatedComment: UpdatedComment, id: string){
+    return this.http.put(environment.apiBaseUrl + '/comment/' + id, updatedComment)
+  }
+
+  deleteComment(id: string){
+   return this.http.delete(environment.apiBaseUrl + '/comment/' + id)
+  }
+
+  postComment(comment: Comment){
+    return this.http.post(environment.apiBaseUrl + '/comment/create',comment)
+  }
+}
