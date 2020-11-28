80de4cd914be57216c95621869e735357d87a348
Gabrysia <gfraczek1999@gmail.com>
2020-11-28 18:33:34 +0100

    dashboard style changes

--- client-side/src/app/dashboard/dashboard.component.ts
+++ client-side/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {UserService} from "../core/user/user.service";
 import {MatDialog} from "@angular/material/dialog";
-import { BookshelvesComponent} from "../bookshelves/bookshelves.component";
+import {BookshelvesComponent} from "../bookshelves/bookshelves.component";
 import {BookshelfService} from "../core/bookshelves/bookshelf.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-dashboard',
@@ -12,8 +13,8 @@ import {BookshelfService} from "../core/bookshelves/bookshelf.service";
 export class DashboardComponent extends BookshelvesComponent {
 
 
-  constructor(protected userService: UserService, protected dialog: MatDialog, protected bookshelfService: BookshelfService) {
-    super(dialog, userService, bookshelfService)
+  constructor(protected userService: UserService, protected dialog: MatDialog, protected bookshelfService: BookshelfService, protected snackBar: MatSnackBar, protected cdr: ChangeDetectorRef) {
+    super(dialog, userService, bookshelfService, snackBar, cdr)
   }
 
 }
