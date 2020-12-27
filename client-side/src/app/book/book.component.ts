9aaec3070c3e19af6908afdf30834882fa7823b4
Gabrysia <gfraczek1999@gmail.com>
2020-12-27 15:41:01 +0100

    Book component fixes

--- client-side/src/app/book/book.component.ts
+++ client-side/src/app/book/book.component.ts
@@ -1,53 +1,54 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {BooksService} from "../core/books/books.service";
-import {UserService} from "../core/user/user.service";
-import {ModifiedUser, User} from "../core/user/user.model";
-import {PreviewBook} from "../core/books/book.model";
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {MatDialog} from "@angular/material/dialog";
-import {AddToBookshelfDialogComponent} from "../shared/components/add-to-bookshelf-dialog/add-to-bookshelf-dialog.component";
-import {BookshelfService} from "../core/bookshelves/bookshelf.service";
-import {SharedService} from "../core/shared/shared.service";
-import {Movie} from "../core/shared/movie.model";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BooksService } from '../core/books/books.service';
+import { UserService } from '../core/user/user.service';
+import { ModifiedUser, User } from '../core/user/user.model';
+import { PreviewBook } from '../core/books/book.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { AddToBookshelfDialogComponent } from '../shared/components/add-to-bookshelf-dialog/add-to-bookshelf-dialog.component';
+import { BookshelfService } from '../core/bookshelves/bookshelf.service';
+import { SharedService } from '../core/shared/shared.service';
+import { Movie } from '../core/shared/movie.model';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
 })
 export class BookComponent implements OnInit {
-  movies: Movie[] = []
+  movies: Movie[] = [];
   book: any;
   id: string;
   user: User;
 
-  constructor(private route: ActivatedRoute,
-              protected booksService: BooksService,
-              protected userService: UserService,
-              protected snackBar: MatSnackBar,
-              protected dialog: MatDialog,
-              protected bookshelfService: BookshelfService,
-              protected sharedService: SharedService
-  ) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    protected booksService: BooksService,
+    protected userService: UserService,
+    protected snackBar: MatSnackBar,
+    protected dialog: MatDialog,
+    protected bookshelfService: BookshelfService,
+    protected sharedService: SharedService
+  ) {}
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id')
+    this.id = this.route.snapshot.paramMap.get('id');
     if (this.id) {
       this.booksService.getBookById(this.id).subscribe((result: any) => {
-        this.book = result
-        if(this.book){
-          this.sharedService.getMovies(this.book.volumeInfo.title).subscribe((result: any)=>{
-            this.movies = result
-          })
+        this.book = result;
+        if (this.book) {
+          this.sharedService
+            .getMovies(this.book.volumeInfo.title)
+            .subscribe((result: any) => {
+              this.movies = result;
+            });
         }
-      })
-
+      });
     }
     this.userService.getUserData().subscribe((result: any) => {
-      this.user = result.user
-    })
+      this.user = result.user;
+    });
   }
 
   getRatingValue(value) {
@@ -55,72 +56,75 @@ export class BookComponent implements OnInit {
   }
 
   checkIfLikedOrRead(checkValue: string): boolean {
-
-    if(this.book){
+    if (this.book) {
       if (checkValue === 'liked') {
-        return this.user?.liked?.some(x => x.id_book === this.book?.id);
+        return this.user?.liked?.some((x) => x.id_book === this.book?.id);
       } else if (checkValue === 'read') {
-        return this.user?.read?.some(x => x.id_book === this.book?.id);
+        return this.user?.read?.some((x) => x.id_book === this.book?.id);
       }
     }
   }
 
-  openDialog(){
+  openDialog() {
     const dialogRef = this.dialog.open(AddToBookshelfDialogComponent, {
       width: '400px',
       height: '400px',
-      data: {id: [], id_user: this.user._id }
+      data: { id: [], id_user: this.user._id, id_book: this.id },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        this.addToBookshelf(result)
+        this.addToBookshelf(result);
       }
     });
   }
 
-  addToBookshelf(id: string[]){
-    id.forEach((id:string)=> {
-      let previewBook = new PreviewBook;
+  addToBookshelf(id: string[]) {
+    id.forEach((id: string) => {
+      let previewBook = new PreviewBook();
       previewBook.id_book = this.book?.id;
       previewBook.image = this.book.volumeInfo.imageLinks.smallThumbnail;
-      previewBook.authors =this.book.volumeInfo.authors;
+      previewBook.authors = this.book.volumeInfo.authors;
       previewBook.title = this.book.volumeInfo.title;
-      this.bookshelfService.addBookToBookshelf(previewBook, id).subscribe(
-        ((result:any)=>{
-          console.log(result)
-        })
-      )
+      this.bookshelfService
+        .addBookToBookshelf(previewBook, id)
+        .subscribe((result: any) => {
+          console.log(result);
+        });
     });
   }
 
   addToLikedOrRead(action: string) {
-    const modifiedUser = new ModifiedUser;
-    const newBook = new PreviewBook;
+    const modifiedUser = new ModifiedUser();
+    const newBook = new PreviewBook();
 
-    newBook.id_book = this.book?.id
-    newBook.authors = this.book?.volumeInfo?.authors
-    newBook.title = this.book?.volumeInfo?.title
-    newBook.image = this.book?.volumeInfo?.imageLinks?.smallThumbnail
+    newBook.id_book = this.book?.id;
+    newBook.authors = this.book?.volumeInfo?.authors;
+    newBook.title = this.book?.volumeInfo?.title;
+    newBook.image = this.book?.volumeInfo?.imageLinks?.smallThumbnail;
 
-    modifiedUser.points = this.user.points
-    modifiedUser.id = this.user._id
-    modifiedUser.liked = this.user.liked
-    modifiedUser.read = this.user.read
+    modifiedUser.points = this.user.points;
+    modifiedUser.id = this.user._id;
+    modifiedUser.liked = this.user.liked;
+    modifiedUser.read = this.user.read;
 
     if (action === 'liked') {
-      modifiedUser.liked.push(newBook)
+      modifiedUser.liked.push(newBook);
     } else if (action === 'read') {
-      modifiedUser.read.push(newBook)
+      modifiedUser.read.push(newBook);
+      modifiedUser.points += 3;
     }
     this.userService.modifyUser(modifiedUser).subscribe(
       () => {
-        this.snackBar.open('List succesfully updated', 'OK', {duration: 2000});
+        this.snackBar.open('List succesfully updated', 'OK', {
+          duration: 2000,
+        });
       },
       () => {
-        this.snackBar.open('List could not be updated', 'OK', {duration: 4000});
+        this.snackBar.open('List could not be updated', 'OK', {
+          duration: 4000,
+        });
       }
-    )
-
+    );
   }
 }
